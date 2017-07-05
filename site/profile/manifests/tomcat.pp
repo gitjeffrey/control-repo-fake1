cc9947481e38467237eb0090b9c78aa7356674da
Jeffrey Frelick <jeff@autostructure.io>
2017-07-05 13:57:29 -0400

    Trying to fix tomcat install.

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -1,13 +1,16 @@
 class profile::tomcat {
 
-  file {'testfile-tomcat':
-    ensure =>  present,
-    path   =>  '/tmp/testfile-tomcat.txt',
-    mode   =>  '0777'
+  tomcat::install { '/opt/tomcat8':
+    source_url => 'https://www.apache.org/dist/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz'
+  }
+  tomcat::instance { 'default':
+   catalina_home => '/opt/tomcat',
   }
 
-  tomcat::install { '/opt/tomcat8':
-    source_url => 'https://www.apache.org/dist/tomcat/tomcat-8/v8.0.x/bin/apache-tomcat-8.0.x.tar.gz'
-  }
+    file {'testfile-tomcat':
+      ensure =>  present,
+      path   =>  '/tmp/testfile-tomcat.txt',
+      mode   =>  '0777'
+    }
 
 }
