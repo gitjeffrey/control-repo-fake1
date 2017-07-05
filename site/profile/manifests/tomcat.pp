9793343215a78ff0b0e4867b95f51cc1553ba991
Jeffrey Frelick <jeff@autostructure.io>
2017-07-05 14:18:17 -0400

    Wrong directory entered.

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -1,6 +1,13 @@
 class profile::tomcat {
 
-  tomcat::install { '/opt/tomcat8':
+  # create the tomcat base directory...
+  file {'create-tomcat-dir':
+    ensure =>  directory,
+    path   =>  '/opt/tomcat',
+    mode   =>  '0777'
+  }
+
+  tomcat::install { '/opt/tomcat':
     source_url => 'https://www.apache.org/dist/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz'
   }
   tomcat::instance { 'default':
