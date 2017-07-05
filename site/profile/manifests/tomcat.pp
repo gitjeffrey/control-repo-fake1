329dd85bf3f12dc52db8beee48984ea88108e122
Jeffrey Frelick <jeff@autostructure.io>
2017-07-05 11:30:47 -0400

    Corrected error, missing commas

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -1,9 +1,9 @@
 class profile::tomcat {
 
   file {'testfile1':
-    path    =>  '/tmp/testfile1.txt'
-    ensure  =>  present
-    mode    =>  '777'
+    ensure =>  present,
+    path   =>  '/tmp/testfile1.txt',
+    mode   =>  '0777',
   }
 
 }
